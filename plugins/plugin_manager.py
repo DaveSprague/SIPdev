c6e826fde03d4ba3d17613db9b4bf83bf87f80c5
David L Sprague <david.sprague@gmail.com>
2017-04-23 18:55:01 -0700

    Changed location of plugins repository to be my fork with flow_sensor plugin available

--- plugins/plugin_manager.py
+++ plugins/plugin_manager.py
@@ -61,7 +61,7 @@ def parse_manifest(plugin):
 
 
 def get_readme():
-    response = urllib.urlopen('https://api.github.com/repos/Dan-in-CA/SIP_plugins/readme')
+    response = urllib.urlopen('https://api.github.com/repos/DaveSprague/SIP_plugins/readme')
     data = response.read()
     d = json.loads(data)
     text = base64.b64decode(d['content'])
@@ -153,7 +153,7 @@ class install_plugins(ProtectedPage):
         for p in qdict.keys():  # Get plugins to install
             print p
 #           https://raw.github.com/<username>/<repo>/<branch>/some_directory/file.r #### Example
-            response = urllib.urlopen('https://raw.github.com/Dan-in-CA/SIP_plugins/master/'+p+'/'+p+'.manifest')
+            response = urllib.urlopen('https://raw.github.com/DaveSprague/SIP_plugins/master/'+p+'/'+p+'.manifest')
             data = response.readlines()
             sep = [i for i, s in enumerate(data) if '###' in s][0]
             file_list = [line.strip() for line in data[sep+2:]]
@@ -162,7 +162,7 @@ class install_plugins(ProtectedPage):
                 new_mf.writelines(data)
             for f in short_list:
                 pf = f.split()
-                response = urllib.urlopen('https://raw.github.com/Dan-in-CA/SIP_plugins/master/'+p+'/'+pf[0])
+                response = urllib.urlopen('https://raw.github.com/DaveSprague/SIP_plugins/master/'+p+'/'+pf[0])
                 f_data = response.read()
                 with open(pf[1]+'/'+pf[0], 'w') as next_file:
                     next_file.write(f_data)
